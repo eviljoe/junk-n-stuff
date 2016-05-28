39a9059e9f035a441dbe1c8c150c3a003ad7c41e
eviljoe <eviljoe@users.noreply.github.com>
2016-05-28 10:09:37 -0400

    [closes #79] Fixed problem with time taken
    Now displaying the correct minutes value for time taken for "uuu.py" updates.

--- src/jnscommons/jnsstr.py
+++ src/jnscommons/jnsstr.py
@@ -11,8 +11,10 @@ def seconds_to_minutes_and_seconds(seconds):
             string = '<{}1s'.format(negative)
         else:
             seconds = round(abs(seconds))
+            
             s = seconds % _SECONDS_IN_MINUTE
-            m = seconds - s
+            m = (seconds - s) / _SECONDS_IN_MINUTE
+            
             string = '{}{}m {}s'.format(negative, m, s) if m > 0 else '{}{}s'.format(negative, s)
     
     return string
