79bf69c7879ce7b189673049aced8ec5bf36a2a6
eviljoe <eviljoe@users.noreply.github.com>
2016-07-13 20:23:51 -0400

    [closes #93] Added system monitor script to tmux
    The results of the system monitor script are now displayed in tmux's status bar.

--- src/sysmonitor.py
+++ src/sysmonitor.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 
+import time
+
+
 def main():
-    print("Mem: {}%, CPU: {}".format(get_memory_usage(), get_cpu_usage()))
+    print("Mem: {}%, CPU: {}%".format(get_memory_usage(), get_cpu_usage()))
 
 
 ##########################
@@ -44,16 +47,67 @@ def _read_meminfo():
 
 
 def get_cpu_usage():
-    return None
+    cpu_info_1 = _read_cpu_stat()
+    cpu_info_2 = None
+    delta_idle = None
+    delta_total = None
+    usage = None
+    
+    time.sleep(0.25)
+    cpu_info_2 = _read_cpu_stat()
+    
+    delta_idle = cpu_info_2.idle - cpu_info_1.idle
+    delta_total = cpu_info_2.total - cpu_info_1.total
+    usage = ((delta_total - delta_idle) / delta_total) * 100
+    
+    return round(usage)
 
 
-#########################
-# System Load Functions #
-#########################
+def _read_cpu_stat():
+    cpu_info = None
+    
+    with open('/proc/stat', 'r') as f:
+        line = f.readline()
+        
+        while cpu_info is None and line is not None:
+            if line.startswith('cpu '):
+                cpu_info = _read_cpu_stat_line(line)
+            
+            line = f.readline()
+    
+    return cpu_info
 
 
-def get_system_load():
-    pass  # JOE read from /proc/loadavg
+def _read_cpu_stat_line(line):
+    parts = line.split()
+    
+    return CPUInfo(
+        user=int(parts[1]),
+        system=int(parts[2]),
+        nice=int(parts[3]),
+        idle=int(parts[4]),
+        wait=int(parts[5]),
+        irq=int(parts[6]),
+        srq=int(parts[7]),
+        zero=int(parts[8]))
+
+
+###########
+# Classes #
+###########
+
+
+class CPUInfo:
+    def __init__(self, user, system, nice, idle, wait, irq, srq, zero):
+        self.user = user
+        self.system = system
+        self.nice = nice
+        self.idle = idle
+        self.wait = wait
+        self.irq = irq
+        self.srq = srq
+        self.zero = zero
+        self.total = user + system + nice + idle + wait + irq + srq + zero
 
 
 if __name__ == '__main__':
